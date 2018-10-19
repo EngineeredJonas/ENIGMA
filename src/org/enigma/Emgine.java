1fe80378be08494ff7d5e95f2d21f0220e3a0463
Jonas <jonrec04@freenet.de>
2018-10-19 16:36:19 +0200

    GUI for the Emgine.

--- src/org/enigma/Emgine.java
+++ src/org/enigma/Emgine.java
@@ -1,11 +1,48 @@
 package org.enigma;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+
 public class Emgine {
 
-    public static void main(final String[] args) {
-        final EncoderDecoder encoderDecoder = new FancyDecoderEncoder();
+    public static void main(final String[] args) throws IOException {
+//        final EncoderDecoder encoderDecoder = new FancyDecoderEncoder();
+//
+//        System.out.println(encoderDecoder.encode("Hello world"));
 
-        System.out.println(encoderDecoder.encode("Hello world"));
+        new JFrame() {
+            {
+                setTitle("Emgine");
+                setFont(Font.getFont(Font.SANS_SERIF));
+                setSize(1000, 500);
+                setLocationRelativeTo(null);
+                setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+                setBackground(Color.cyan);
+                getContentPane().setBackground(Color.CYAN);
+                setLayout(null);
+                setIconImage(ImageIO.read(ClassLoader.getSystemResourceAsStream("envelope.png")));
+
+                add(new JButton(new AbstractAction("Foobar") {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        System.out.println("Hallo");
+                    }
+                }) {
+                    {
+                        setSize(80, 70);
+                        setLocation(500, 400);
+                        setText("Hallo");
+
+
+
+                    }
+                });
+
+            }
+        }.show();
     }
-
 }
+
