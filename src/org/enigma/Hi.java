e05ab431c4785bb6b4af3e0451df22245864e2e8
Lennart73538345429 <lennartzipfel9@gmail.com>
2018-10-19 17:37:12 +0200

    eingabe der Variable und Ausgabe des Unicorn Codes

--- src/org/enigma/Hi.java
+++ src/org/enigma/Hi.java
@@ -1,10 +1,11 @@
 package org.enigma;
-
-public class Hi {
+public class Hi{
         public static void  main(final String[] args) {
-            char c = 'A';
+         char c = 'A'
             int i = (int) c;
-            System.out.println(i);
+         while(i)
+
+
 
         }
 
