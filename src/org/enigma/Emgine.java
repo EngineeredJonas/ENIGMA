6304fb3b1f249bb8ea0cd0d96e8606359e9430b4
Jonas <jonrec04@freenet.de>
2018-10-20 11:31:31 +0200

    nö

--- src/org/enigma/Emgine.java
+++ src/org/enigma/Emgine.java
@@ -1,5 +1,6 @@
 package org.enigma;
 //Modules
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
@@ -9,19 +10,19 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 public class Emgine {
+//private Image backgroundImage;
+    public static void main(final String[] args) throws IOException, FontFormatException {
+        final EncoderDecoder encoderDecoder = new FancyDecoderEncoder();
+        //backgroundImage = ImageIO.read(new File());
 
-    public static void main(final String[] args) throws IOException {
-//        final EncoderDecoder encoderDecoder = new FancyDecoderEncoder();
-//
 //        System.out.println(encoderDecoder.encode("Hello world"));
         //Fonts
         Font CENA = new Font("SansSerif", Font.BOLD, 30);
         Font Input = new Font("toll", Font.BOLD, 30);
         Font Button = new Font("Butt", Font.BOLD, 25);
-        //File font_file = new File("ChakraPetch-Light.ttf");
-        //Font font = Font.createFont(Font.TRUETYPE_FONT, font_file);
-        //TODO ChakraPetch. ttf verwenden
+        Font font = Font.createFont(Font.TRUETYPE_FONT, ClassLoader.getSystemResourceAsStream("ChakraPetch-Light.ttf")).deriveFont(30f);
 
+        //TODO ChakraPetch. ttf verwenden
 
 
         new JFrame() {
@@ -29,19 +30,63 @@ public class Emgine {
                 setTitle("Emgine");
                 setFont(Font.getFont(Font.SANS_SERIF));
                 setSize(1000, 500);
+                setMinimumSize(new Dimension(1000, 500));
                 setLocationRelativeTo(null);
                 setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
                 setBackground(Color.cyan);
                 getContentPane().setBackground(Color.CYAN);
                 setLayout(null);
                 setIconImage(ImageIO.read(ClassLoader.getSystemResourceAsStream("padlock.png")));
+                setResizable(false);
 
                 // TODO Backgroundimage
 
-                add(new JButton(new AbstractAction("Foobar") {
+                JTextField inputField = new JTextField(80) {
+                    {   //Eingabefeld
+                        setSize(500, 50);
+                        setLocation(250, 150);
+                        setFont(font);
+                    }
+                };
+
+
+                ;
+
+
+                add(inputField);
+
+                add(new JLabel("Eingabe") {
+                    {  //Anzeigefeld für Input
+                        setSize(100, 50);
+                        setLocation(250, 80);
+                        setText(" Input");
+                        setFont(Input);
+                    }
+                });
+
+                JTextField outputField = (new JTextField(80) {
+                    {
+                        //Outputfeld
+                        setSize(500, 50);
+                        setLocation(250, 300);
+                        setFont(font);
+                        setTitle("Output");
+                        setEditable(false);
+                    }
+                });
+                add(outputField);
+
+                JButton Button1 = ((new JButton(new AbstractAction("Foobar") {
                     @Override
-                    public void actionPerformed(ActionEvent e) {
-                        System.out.println("button funktioniert");
+                    public void actionPerformed(ActionEvent ae) {
+                        String textFieldValue = inputField.getText();
+                        System.out.println(textFieldValue);
+                        if (textFieldValue.length() == 0) {
+                            outputField.setText("");
+                        } else {
+                            String Encoded = encoderDecoder.encode(textFieldValue);
+                            outputField.setText(Encoded);
+                        }
 
 
                     }
@@ -53,41 +98,27 @@ public class Emgine {
                         setFont(Button);
 
 
+                    }
+                }));
+                add(Button1);
 
+
+                add(new JLabel("Ausgabe") {
+                    {  //Anzeigefeld für Output
+                        setSize(110, 50);
+                        setLocation(250, 230);
+                        setText(" Output");
+                        setFont(Input);
                     }
                 });
 
-
-                //TODO TextFeld variable !
-                add(new JTextField(80) {
-                        {   //Eingabefeld
-                            setSize(500, 50);
-                            setLocation(250, 150);
-                            setFont(CENA);
-                            //Nur Buchstaben und Nummern
-                            //String text = "axc";
-                            //boolean match = text.matches("[a-zA-Z0-9]+");
+                getRootPane().setDefaultButton(Button1);
 
 
+            }
 
+            ;
 
-
-
-                        }
-                    });
-
-                add(new JTextField(80) {
-                    {  //Anzeigefeld für Input
-                       setSize(100, 50);
-                       setLocation(250,80);
-                       setEditable(false);
-                       setText(" Input");
-                       setFont(Input);
-                    }
-                });
-
-
-            };
         }.show();
     }
 }
