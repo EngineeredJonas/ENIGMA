b621058395596b6fe962c567d43dd5e92b0665d0
Lennart73538345429 <lennartzipfel9@gmail.com>
2018-10-20 15:32:08 +0200

    Programm was die Buchstaben im Alphabet verschiebt und auch wieder decodiert.

--- src/org/enigma/AESEncoderDecoder.java
+++ src/org/enigma/AESEncoderDecoder.java
@@ -1,20 +1,41 @@
 package org.enigma;
-
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
 import java.util.Base64;
 
-public class AESEncoderDecoder implements EncoderDecoder
-{
+public class AESEncoderDecoder implements EncoderDecoder {
+
     private SecureRandom rng = new SecureRandom();
+    private byte[] salt = "constant salt".getBytes(StandardCharsets.US_ASCII);
     private SecretKey secretKey = new SecretKeySpec(
             rng.generateSeed(16),
             "AES");
 
+
+    public void passwort(String input){
+
+
+        try {
+            SecretKeyFactory keyFactory =
+                    SecretKeyFactory.getInstance("PBKDF2WithHmacSHA384");
+            KeySpec keySpec = new PBEKeySpec(input.toCharArray(), salt, 100000, 128);
+            SecretKey pbeKey =  keyFactory.generateSecret(keySpec);
+            secretKey = new SecretKeySpec(pbeKey.getEncoded(), "AES");
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            e.printStackTrace();
+
+        }
+
+    }
     @Override
     public String encode(String input) {
         try {
@@ -52,14 +73,15 @@ public class AESEncoderDecoder implements EncoderDecoder
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         EncoderDecoder cipher = new AESEncoderDecoder();
+        cipher.passwort("secret");
 
         String encrypted = cipher.encode("Hello World!");
-        System.out.println(encrypted);
+        System.out.println("encrypted: " + encrypted);
 
         String decrypted = cipher.decode((encrypted));
-        System.out.println(decrypted);
+        System.out.println("decrypted: "+decrypted);
 
     }
 }
--- src/org/enigma/EncoderDecoder.java
+++ src/org/enigma/EncoderDecoder.java
@@ -3,7 +3,7 @@ package org.enigma;
 public interface EncoderDecoder {
 
     String encode(String input);
-    default void Passwort (String minput){
+    default void passwort (String input){
 
     }
     String decode(String input);
